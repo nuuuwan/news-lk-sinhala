7e8890306ee3536d2b4a197fad92737bb7fc09a8
nuuuwan <nuuuwan@gmail.com>
2021-10-20 11:26:34 +0530

    [main]  (2021-10-20-1126)

--- src/news_lk_si/scrape.py
+++ src/news_lk_si/scrape.py
@@ -19,12 +19,12 @@ def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
-
 def get_name_base(url):
     h = hashx.md5(url)[:N_HASH]
     date_str = url[24:34]
     date_id = date_str.replace('/', '')
     return f'news_lk_si.{NEWS_PAPER_NAME}.{date_id}.{h}'
+
 
 
 def scrape(url):
@@ -59,36 +59,42 @@ def scrape(url):
 def text_to_audio(text_file):
     paragraphs = filex.read(text_file).split('\n\n')
     file_base = text_file[:-4]
-    tmp_base = file_base[5:]
-
-    n = len(paragraphs)
-    audio_files = []
-    for i, paragraph in enumerate(paragraphs):
-        i1 = i + 1
-        audio_file = f'/tmp/tmp_{tmp_base}.{i1:03d}.mp3'
-        if os.path.exists(audio_file):
-            log.warning(f'{audio_file} exists. Aborting TTS.')
-        else:
-            text = paragraph.replace('# ', '')
-            try:
-                tts = gTTS(text, lang=LANG)
-                tts.save(audio_file)
-                log.info(f'Wrote {i1}/{n} to {audio_file}')
-            except:
-                continue
-        audio_files.append(audio_file)
-
-    combined_audio = None
-    for audio_file in audio_files:
-        audio = AudioSegment.from_file(audio_file)
-
-        if combined_audio is None:
-            combined_audio = audio
-        else:
-            combined_audio += audio
     combined_audio_file = f'{file_base}.mp3'
-    combined_audio.export(combined_audio_file)
-    log.info(f'Wrote combined audio to {combined_audio_file}')
+
+    if os.path.exists(combined_audio_file):
+        log.warning(f'{combined_audio_file} exists. Aborting TTS.')
+
+    else:
+        tmp_base = file_base[5:]
+
+        n = len(paragraphs)
+        audio_files = []
+        for i, paragraph in enumerate(paragraphs):
+            i1 = i + 1
+            audio_file = f'/tmp/tmp_{tmp_base}.{i1:03d}.mp3'
+            if os.path.exists(audio_file):
+                log.warning(f'{audio_file} exists. Aborting TTS.')
+            else:
+                text = paragraph.replace('# ', '')
+                try:
+                    tts = gTTS(text, lang=LANG)
+                    tts.save(audio_file)
+                    log.info(f'Wrote {i1}/{n} to {audio_file}')
+                except:
+                    continue
+            audio_files.append(audio_file)
+
+        combined_audio = None
+        for audio_file in audio_files:
+            audio = AudioSegment.from_file(audio_file)
+
+            if combined_audio is None:
+                combined_audio = audio
+            else:
+                combined_audio += audio
+        combined_audio.export(combined_audio_file)
+        log.info(f'Wrote combined audio to {combined_audio_file}')
+    return combined_audio_file
 
 
 def get_urls():
@@ -105,10 +111,8 @@ def get_urls():
 
 
 def process(url):
-    log.info(f'Processing "{url}"...')
     text_file = scrape(url)
     text_to_audio(text_file)
-
 
 def is_alread_parsed(url):
     name_base = get_name_base(url)
@@ -125,7 +129,7 @@ def process_all():
     n = len(urls)
     for i, url in enumerate(urls):
         i1 = i + 1
-        log.info(f'{i1}/{n} Processing "{url}"...')
+        log.info(f'Processing {i1}/{n} "{url}"...')
         if is_alread_parsed(url):
             log.warning(f'{url} already processed. Aboring process')
         else:
